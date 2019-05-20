0ab9853c339ce4ce71a5d68cefd7f9faa21e77de
Dimitri C <couwenberg@gmail.com>
2019-05-20 12:51:35 +1000

    flex stuff start

---
+++ src/FlexContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class FlexContainer extends Component {
+  render() {
+    return (
+      <div class="flex-container">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+      </div>
+    );
+  }
+}
