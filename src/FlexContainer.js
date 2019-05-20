7ff94171e6c570ade988b1a7732f64f6a6269aca
Dimitri C <couwenberg@gmail.com>
2019-05-20 14:27:16 +1000

    update

--- src/FlexContainer.js
+++ src/FlexContainer.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
 //demo for basic flex and wrap option
 
-import './flexcontainer.css';
+import "./flexcontainer.css";
 
 export default class FlexContainer extends Component {
   render() {
     return (
-      <div class="flex-container">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
+      <div className="flex-container">
+        <div className="div1">1</div>
+        <div className="div1">2</div>
+        <div className="div2">3</div>
+        <div className="div2">4</div>
       </div>
     );
   }
